7c4aa9e286c1415e81d50fc746db483e7d92bc57
HaoDaWang <1306036576@qq.com>
2019-05-21 15:22:59 +0800

    [Feat] add new model for user

---
+++ models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           string `gorm:"type:uuid;primary_key;default:uuid_generate_v4"`
+	DisplayName  string `gorm:"type:varchar(12)"`
+	Username     string `gorm:"type:varchar(20);unique"`
+	Password     string `gorm:"type:char(32)"`
+	Email        string `gorm:"type:varchar(64)"`
+	Avatar       string `gorm:"type:varchar(200)"`
+	RegisterTime *time.Time
+	LoginTime    *time.Time
+}
