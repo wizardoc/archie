e056ea7ec1e7c90fee68e4b8f06ffa8e0233b2e3
HaoDaWang <1306036576@qq.com>
2019-05-28 22:28:51 +0800

    [Feat] add handler

--- utils/error-checker.go
+++ utils/error-checker.go
@@ -1,9 +1,15 @@
 package utils
 
-import "log"
+import (
+	"log"
+)
 
-func Check(err error){
+func Check(err error, handler ...func()) {
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if len(handler) != 0 {
+		handler[0]()
 	}
 }
