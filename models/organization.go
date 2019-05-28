4d1622332b6a79e8c2262192429e01d979c65c2c
HaoDaWang <1306036576@qq.com>
2019-05-28 22:54:59 +0800

    [Feat] wrap organization

--- models/organization.go
+++ models/organization.go
@@ -1,12 +1,33 @@
 package models
 
-import "time"
+import (
+	"archie/connection"
+	"archie/utils"
+	"time"
+)
 
 type Organization struct {
 	ID           string  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	OrganizeName string  `gorm:"type:varchar(20)"`
 	Description  string  `gorm:"type:varchar(50)"`
-	HasValid     bool    `gorm:"type:bool"`
+	HasValid     bool    `gorm:"type:bool;default:TRUE"`
 	Users        *[]User `gorm:"many2many:user_organizations;"`
-	CreateTime   *time.Time
+	CreateTime   time.Time
+}
+
+func (organization *Organization) NewOrganization() (success bool) {
+	db, err := connection.GetDB()
+
+	utils.Check(err, func() {
+		success = false
+	})
+
+	defer db.Close()
+
+	organization.HasValid = true
+	organization.CreateTime = time.Now()
+
+	db.Create(organization)
+
+	return
 }
