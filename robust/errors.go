fc845faf01afccd75d2cf62dba68bed9c69fb28f
HaoDaWang <1306036576@qq.com>
2019-07-18 18:03:53 +0800

    [Feat] wrap CONNOT_CREATE_ORGANIZATION

--- robust/errors.go
+++ robust/errors.go
@@ -3,3 +3,4 @@ package robust
 import "errors"
 
 var CANNOT_FIND_ORGANIZATION = errors.New("cannot find organization")
+var CONNOT_CREATE_ORGANIZATION = errors.New("could't create new organization")
