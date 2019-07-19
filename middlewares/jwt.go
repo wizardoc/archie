d6a1d0537c4b6a1df7d7529a9212d5dfa10ebc39
HaoDaWang <1306036576@qq.com>
2019-07-19 21:02:23 +0800

    [Feat] add ParseToken

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -1,1 +1,21 @@
 package middlewares
+
+import (
+	"archie/utils"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func ParseToken(tokenStr string) jwt.Claims {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return utils.GetSecretKey(), nil
+	})
+
+	utils.Check(err)
+
+	return token.Claims
+}
+
+func validateToken(context *gin.Context) {
+
+}
