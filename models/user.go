284b0cffce812647baba85530c3877ed316989dd
HaoDaWang <1306036576@qq.com>
2019-08-07 09:55:02 +0800

    [Feat] wrap user model

--- models/user.go
+++ models/user.go
@@ -1,17 +1,72 @@
 package models
 
 import (
+	"archie/connection"
+	"archie/utils"
+	"fmt"
 	"time"
 )
 
 type User struct {
-	ID            string          `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	DisplayName   string          `gorm:"type:varchar(12)"`
-	Username      string          `gorm:"type:varchar(20);unique;"`
-	Password      string          `gorm:"type:char(32)"`
-	Email         string          `gorm:"type:varchar(64)"`
-	Avatar        string          `gorm:"type:varchar(200)"`
-	Organizations *[]Organization `gorm:"many2many:user_organizations;"`
-	RegisterTime  *time.Time
-	LoginTime     *time.Time
+	ID            string          `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"json:"-"`
+	DisplayName   string          `gorm:"type:varchar(12)"json:"displayName"`
+	Username      string          `gorm:"type:varchar(20);unique;"json:"username"`
+	Email         string          `gorm:"type:varchar(64)"json:"email"`
+	Avatar        string          `gorm:"type:varchar(200)"json:"avatar"`
+	Organizations *[]Organization `gorm:"many2many:user_organizations;"json:"-"`
+	RegisterTime  int64           `gorm:"type:bigint"json:"registerTime"`
+	LoginTime     int64           `gorm:"type:bigint"json:"loginTime"`
+	Password      string          `gorm:"type:char(62)"json:"-"`
+}
+
+func (user *User) Register() (ok bool) {
+	db, err := connection.GetDB()
+
+	utils.Check(err)
+	defer db.Close()
+
+	user.RegisterTime = time.Now().Unix()
+	// make more security password
+	user.Password = utils.Hash(user.Password)
+
+	db.Create(user)
+
+	return true
+}
+
+func (user *User) UpdateLoginTime() {
+	db, err := connection.GetDB()
+
+	utils.Check(err)
+	defer db.Close()
+
+	db.Model(&user).Where("id = ?", user.ID).Update("login_time", time.Now().Unix())
+}
+
+func (user *User) GetUserInfoByID() User {
+	db, err := connection.GetDB()
+
+	utils.Check(err)
+	defer db.Close()
+
+	userID := user.ID
+	result := User{}
+
+	db.Find(&result, "id = ?", userID)
+
+	return result
+}
+
+func FindOneByUsername(username string) User {
+	db, err := connection.GetDB()
+
+	utils.Check(err)
+	defer db.Close()
+	user := User{}
+
+	db.Find(&user, "username = ?", username)
+
+	fmt.Println(user, username)
+
+	return user
 }
