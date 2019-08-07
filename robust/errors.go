fadd6a967ec246b68508d9042a2bca45b7ed3615
HaoDaWang <1306036576@qq.com>
2019-08-07 09:55:32 +0800

    [Feat] rewrap error

--- robust/errors.go
+++ robust/errors.go
@@ -1,6 +1,20 @@
 package robust
 
-import "errors"
+type ArchieError struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
 
-var CANNOT_FIND_ORGANIZATION = errors.New("cannot find organization")
-var CONNOT_CREATE_ORGANIZATION = errors.New("could't create new organization")
+var CANNOT_FIND_ORGANIZATION = ArchieError{1001, "cannot find organization"}
+var CONNOT_CREATE_ORGANIZATION = ArchieError{1002, "could't create new organization"}
+
+// Account
+var REGISTER_FAILURE = ArchieError{1003, "register fail"}
+var REGISTER_EXIST_USER = ArchieError{1004, "the user is exist"}
+
+// JWT
+var JWT_DOES_NOT_EXIST = ArchieError{4001, "jwt does not exist"}
+var JWT_PARSE_ERROR = ArchieError{4002, "cannot parse jwt"}
+
+// DB
+var CREATE_DATA_FAILURE = ArchieError{3001, "create data to db failure"}
