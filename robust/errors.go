9006307f8286296ca1fa6e3d8e8014f39a5039ba
HaoDaWang <1306036576@qq.com>
2019-08-12 15:48:36 +0800

    [Update] change error msg

--- robust/errors.go
+++ robust/errors.go
@@ -5,17 +5,18 @@ type ArchieError struct {
 	Msg  string `json:"msg"`
 }
 
-var CANNOT_FIND_ORGANIZATION = ArchieError{1001, "cannot find organization"}
-var CONNOT_CREATE_ORGANIZATION = ArchieError{1002, "could't create new organization"}
+var CANNOT_FIND_ORGANIZATION = ArchieError{1001, "Cannot find organization"}
+var CONNOT_CREATE_ORGANIZATION = ArchieError{1002, "Could't create new organization"}
 
 // Account
-var REGISTER_FAILURE = ArchieError{1003, "register fail"}
-var REGISTER_EXIST_USER = ArchieError{1004, "the user is exist"}
+var REGISTER_FAILURE = ArchieError{1003, "Register fail"}
+var REGISTER_EXIST_USER = ArchieError{1004, "The user is exist"}
+var LOGIN_USER_DOES_NOT_EXIST = ArchieError{1005, "The user does not exist"}
 
 // JWT
-var JWT_DOES_NOT_EXIST = ArchieError{4001, "jwt does not exist"}
-var JWT_PARSE_ERROR = ArchieError{4002, "cannot parse jwt"}
-var JWT_NOT_ALLOWED = ArchieError{4003, "the jwt is not allowed"}
+var JWT_DOES_NOT_EXIST = ArchieError{4001, "Jwt does not exist"}
+var JWT_PARSE_ERROR = ArchieError{4002, "Cannot parse jwt"}
+var JWT_NOT_ALLOWED = ArchieError{4003, "The jwt is not allowed"}
 
 // DB
-var CREATE_DATA_FAILURE = ArchieError{3001, "create data to db failure"}
+var CREATE_DATA_FAILURE = ArchieError{3001, "Create data to db failure"}
