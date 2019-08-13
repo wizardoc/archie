bc523a7183925884d86781c559f2f2b32083569e
HaoDaWang <1306036576@qq.com>
2019-08-13 15:14:25 +0800

    [Feat] add New

--- models/user_organization.go
+++ models/user_organization.go
@@ -1,10 +1,25 @@
 package models
 
-import "time"
+import (
+	"archie/connection"
+	"archie/utils"
+	"time"
+)
 
 type UserOrganization struct {
 	UserID         string `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	OrganizationID string `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	IsOwner        bool   `gorm:"type:bool"`
-	JoinTime       *time.Time
+	JoinTime       int64
+}
+
+func (userOrganization *UserOrganization) New(isOwner bool) {
+	db, err := connection.GetDB()
+
+	utils.Check(err)
+	defer db.Close()
+
+	userOrganization.JoinTime = time.Now().Unix()
+
+	db.Create(userOrganization)
 }
