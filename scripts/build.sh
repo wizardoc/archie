1785feaddfb158c139da13b1e258aecbe7ddfe0d
HaoDaWang <1306036576@qq.com>
2019-10-11 22:21:47 +0800

    [Feat] update build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,6 @@
 DIST_NAME="archie";
 DIST_DIR="../build";
 
-echo "Begin build...\n";
+echo "Begin build...";
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o $DIST_DIR/$DIST_NAME ./main.go;
-
-echo "Begin docker build...\n";
-
-docker build -t archie .
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o $DIST_DIR/$DIST_NAME ../main.go;
