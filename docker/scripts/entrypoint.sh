1bf5b4dad811e7aec98b694082eec31723252fd8
HaoDaWang <1306036576@qq.com>
2019-10-14 22:17:40 +0800

    [Feat] add wait-for-it

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 ENTRY_POINT="../../build/archie"
 
-chmod 777 $ENTRY_POINT;
-
-$ENTRY_POINT;
+/usr/local/bin/wait-for-it.sh postgres:5432 -- chmod 777 $ENTRY_POINT && $ENTRY_POINT
--- dockerfile
+++ dockerfile
@@ -3,6 +3,8 @@ LABEL younccat zzhbbdbbd@163.com
 
 COPY . .
 
+ADD ./docker/scripts/wait-for-it.sh /usr/local/bin/
+
 EXPOSE 3000
 
 ENTRYPOINT ["/docker/scripts/entrypoint.sh"]
