dfa3d51ff23db6e1c3fb4200f3229790faaa8c75
HaoDaWang <1306036576@qq.com>
2019-10-26 17:10:03 +0800

    [Feat] update config for CORS

--- routes/index.go
+++ routes/index.go
@@ -12,7 +12,15 @@ func Serve() {
 	config := configer.LoadServeConfig()
 
 	router := gin.Default()
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowAllOrigins:  false,
+		AllowOriginFunc:  func(origin string) bool { return true },
+		MaxAge:           86400,
+	}))
 
 	userRouter(router)
 	organizationRouter(router)
