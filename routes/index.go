a83ed1cf0ffb40bd6286022dfb9be0799339afaa
HaoDaWang <1306036576@qq.com>
2019-10-30 16:07:48 +0800

    [Fix] disable CORS

--- routes/index.go
+++ routes/index.go
@@ -4,7 +4,6 @@ import (
 	"archie/utils"
 	"archie/utils/configer"
 	"fmt"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,15 +11,23 @@ func Serve() {
 	config := configer.LoadServeConfig()
 
 	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowAllOrigins:  false,
-		AllowOriginFunc:  func(origin string) bool { return true },
-		MaxAge:           86400,
-	}))
+	//router.Use(cors.New(cors.Config{
+	//	AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT"},
+	//	AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
+	//	ExposeHeaders:    []string{"Content-Length"},
+	//	AllowCredentials: true,
+	//	AllowAllOrigins:  false,
+	//	AllowOriginFunc:  func(origin string) bool { return true },
+	//	MaxAge:           86400,
+	//}))
+
+	//router.Use(func(c *gin.Context) {
+	//	for k, v := range c.Request.Header {
+	//		fmt.Println("")
+	//		fmt.Println(k, v)
+	//		fmt.Println("")
+	//	}
+	//})
 
 	userRouter(router)
 	organizationRouter(router)
