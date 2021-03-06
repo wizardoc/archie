c15feae3e2ddf36f38351b5af1f599f579ae4934
youncccat <1306036576@qq.com>
2021-03-07 00:11:01 +0800

    [Feat] rename user resolver & add resolver to get info of organization

--- models/common.go
+++ models/common.go
@@ -4,6 +4,9 @@ import (
 	"archie/connection/postgres_conn"
 )
 
+type AssociationModel interface {
+}
+
 func updateSig(model interface{}, name string, value interface{}) error {
 	return postgres_conn.DB.Instance().Model(model).Update(name, value).Error
 }
